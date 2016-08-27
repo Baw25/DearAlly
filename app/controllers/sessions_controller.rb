a3a51299300e4f4e926b768631548a1bc7d786e4
Connor Ray <connor.ray.cb@gmail.com>
2016-08-27 14:49:22 -0700

    Creates ally controller and view -cr

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+get '/sessions/new' do
+
+  erb :'/sessions/new'
+end
+
+post '/sessions' do
+  @ally = Ally.find_by_email(params[:email])
+  if @ally && @ally.password == params[:password]
+    session[:id] = @ally.id
+    erb :'index' #Where to redirect?
+  else
+    @errors = ["Username && Password not found."]
+    erb :'error'
+  end
+end
+
+delete '/sessions' do
+  session[:id] = nil
+  redirect :'/'
+end
